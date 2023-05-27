c627aa85115dae2aa1566300be97f21824a0713b
Arik Cohen <me@arikcohen.com>
2023-05-26 23:38:20 -0400

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   content: ["*.html"],
   theme: {
     colors: {
-      'cyan': 'hsl(185, 75%, 39%)',
-      'blue':'hsl(229, 23%, 23%)',
-      'grayish-blue': 'hsl(227, 10%, 46%)',
-      'gray': 'hsl(0, 0%, 59%)',
-      'white': 'hsl(0, 0%, 100%)',
+      'color-1': 'hsl(234, 25%, 52%)',
+      'color-2':'hsl(233, 54%, 16%)',
+      'color-3':'hsl(232, 10%, 56%)',
+      'color-4':'hsl(341, 92%, 62%)',
+      'color-5':'hsl(341, 100%, 83%)',
+      'color-6':'hsl(0, 0%, 100%)',
+      'color-7':'hsl(234, 100%, 97%)',
+      'gradient-1-a':'hsl(322, 87%, 55%)',
+      'gradient-1-b':'hsl(13, 100%, 64%)',
+      'gradient-2-a':'hsl(237, 100%, 64%)',
+      'gradient-2-b':'hsl(322, 87%, 55%)',
     },
     extend: {
-      backgroundImage: {
-        'pattern-bottom':"url('images/bg-pattern-bottom.svg')",
-        'pattern-top':"url('images/bg-pattern-top.svg')",
-      },
       fontFamily: {
-        'sans'  : ['Kumbh Sans', 'sans-serif'],
+        'sans'  : ['Plus Jakarta Sans', 'sans-serif'],
       },
     },
   },
